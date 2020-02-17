5a84faa83d07f90cd5b1c31451327bf3bc10303a
João Pedro Paulo <joaopospaulo@gmail.com>
2020-02-17 22:47:48 +0000

    Fixes docker development setup. Adds dummy routes.

--- server/src/server.js
+++ server/src/server.js
@@ -4,8 +4,13 @@ const app = express();
 // Initialise middlewares
 app.use(express.json({ extended: false }));
 
+// Define Routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+//app.use("/api/posts", require("./routes/api/posts"));
+
 app.get("/", (req, res) => {
-  res.send("API is working");
+  res.send("API is work");
 });
 
 const PORT = process.env.PORT || 5000;
