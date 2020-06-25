15dee57ab725b3bec870b1573acfa0d6b05d0383
João Pedro Paulo <joaopospaulo@gmail.com>
2020-06-26 00:54:10 +0100

    Starts restyling and refactoring of EditProfile component.

--- client/src/components/profile/EditProfile.js
+++ client/src/components/profile/EditProfile.js
@@ -1,15 +1,23 @@
 import React, { useState, useEffect, Fragment } from "react";
+import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { Typography, TextField, Button, Link } from "@material-ui/core";
-
+import {
+  Typography,
+  TextField,
+  Button,
+  Link,
+  Checkbox,
+  Grid,
+  Container,
+  Box,
+  Paper,
+} from "@material-ui/core";
 import { createUpdateProfile, getCurrentProfile } from "../../actions/profile";
-
 import AddSkillForm from "./skills/AddSkillForm";
 import SkillList from "./skills/SkillList";
-
 import CreateUpdateSocialMediaInputs from "./social-media/CreateUpdateSocialMediaInputs";
+import useStyles from "../../styles/Styles";
 
 const EditProfile = ({
   profile: { profile, loading },
@@ -17,23 +25,6 @@ const EditProfile = ({
   getCurrentProfile,
   history,
 }) => {
-  // States
-  const [formData, setFormData] = useState({
-    currentCompany: "",
-    location: "",
-    currentJobTitle: "",
-    skills: [],
-    bio: "",
-    youtube: "",
-    instagram: "",
-    linkedin: "",
-    twitter: "",
-    github: "",
-    facebook: "",
-  });
-
-  const [displaySocialInputs, toggleSocialInputs] = useState(false);
-
   useEffect(() => {
     getCurrentProfile();
 
@@ -54,9 +45,28 @@ const EditProfile = ({
     });
   }, [loading]);
 
+  const classes = useStyles();
+
+  // States
+  const [formData, setFormData] = useState({
+    currentCompany: "",
+    location: "",
+    currentJobTitle: "",
+    skills: [],
+    bio: "",
+    youtube: "",
+    instagram: "",
+    linkedin: "",
+    twitter: "",
+    github: "",
+    facebook: "",
+  });
+
+  const [displaySocialInputs, toggleSocialInputs] = useState(false);
+
   const { currentCompany, location, currentJobTitle, skills, bio } = formData;
 
-  // Handle state changes
+  // Update state
   const onRemoveSkill = (index) => {
     let list = [...skills];
     list.splice(index, 1);
@@ -76,84 +86,114 @@ const EditProfile = ({
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
+  // Helper
+  const isProfileUpdate = () => {
+    return currentCompany != "" ||
+      currentJobTitle != "" ||
+      location != "" ||
+      bio != "" ||
+      skills
+      ? true
+      : false;
+  };
+
+  // Actions to be taken to the backend
   const onSubmit = (e) => {
     e.preventDefault();
-    createUpdateProfile(formData, history, true);
+    createUpdateProfile(formData, history, isProfileUpdate());
   };
 
   return (
-    <Fragment>
-      <Typography variant="h3">Create Profile</Typography>
-      <form onSubmit={(e) => onSubmit(e)}>
-        <div>
-          <Typography variant="h6">Current Company</Typography>
-          <TextField
-            id="currentCompany"
-            label="Company"
-            value={currentCompany}
-            onChange={(e) => handleValueChange(e)}
-          />
-        </div>
-        <div>
-          <Typography variant="h6">Current Job Title</Typography>
-          <TextField
-            id="currentJobTitle"
-            label="Job title"
-            value={currentJobTitle}
-            onChange={(e) => handleValueChange(e)}
-          />
-        </div>
-        <div>
-          <Typography variant="h6">Current Location</Typography>
-          <TextField
-            id="location"
-            label="Location"
-            value={location}
-            onChange={(e) => handleValueChange(e)}
-          />
-        </div>
-        <div>
-          <Typography variant="h6">Skills & Experience Levels</Typography>
-          <AddSkillForm onAddSkill={onAddSkill} />
-          <SkillList skillList={skills} onRemoveSkill={onRemoveSkill} />
-        </div>
-        <div>
-          <Typography variant="h6">Biography</Typography>
-          <TextField
-            id="bio"
-            label="A short bio"
-            multiline
-            rows={5}
-            variant="outlined"
-            value={bio}
-            onChange={(e) => handleValueChange(e)}
-          />
-        </div>
-        <div>
-          <Button
-            variant="outlined"
-            color="primary"
-            onClick={() => toggleSocialInputs(!displaySocialInputs)}
-          >
-            Social Network Links
-          </Button>
-        </div>
-        {displaySocialInputs ? (
-          <CreateUpdateSocialMediaInputs
-            handleValueChange={handleValueChange}
-            social={profile.social}
-          />
-        ) : (
-          <Fragment></Fragment>
-        )}
-        <Button type="submit" variant="contained" color="primary">
-          Submit
-        </Button>
-        <Typography>
-          <Link href="/dashboard">Back</Link>
-        </Typography>
-      </form>
-    </Fragment>
+    <Box className={classes.editModeContainers}>
+      <Grid container justify="flex-start" spacing={2}>
+        <Grid item xs={12}>
+          <Box className={classes.editModeHeadersContainers}>
+            {isProfileUpdate() ? (
+              <Typography variant="h3">Edit Profile</Typography>
+            ) : (
+              <Typography variant="h3">Create Profile</Typography>
+            )}
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Paper className={classes.editModePaperContainers}>
+            <Container maxWidth="sm">
+              <form onSubmit={(e) => onSubmit(e)}>
+                <div>
+                  <Typography variant="h6">Current Company</Typography>
+                  <TextField
+                    id="currentCompany"
+                    label="Company"
+                    value={currentCompany}
+                    onChange={(e) => handleValueChange(e)}
+                  />
+                </div>
+                <div>
+                  <Typography variant="h6">Current Job Title</Typography>
+                  <TextField
+                    id="currentJobTitle"
+                    label="Job title"
+                    value={currentJobTitle}
+                    onChange={(e) => handleValueChange(e)}
+                  />
+                </div>
+                <div>
+                  <Typography variant="h6">Current Location</Typography>
+                  <TextField
+                    id="location"
+                    label="Location"
+                    value={location}
+                    onChange={(e) => handleValueChange(e)}
+                  />
+                </div>
+                <div>
+                  <Typography variant="h6">
+                    Skills & Experience Levels
+                  </Typography>
+                  <AddSkillForm onAddSkill={onAddSkill} />
+                  <SkillList skillList={skills} onRemoveSkill={onRemoveSkill} />
+                </div>
+                <div>
+                  <Typography variant="h6">Biography</Typography>
+                  <TextField
+                    id="bio"
+                    label="A short bio"
+                    multiline
+                    rows={5}
+                    variant="outlined"
+                    value={bio}
+                    onChange={(e) => handleValueChange(e)}
+                  />
+                </div>
+                <div>
+                  <Button
+                    variant="outlined"
+                    color="primary"
+                    onClick={() => toggleSocialInputs(!displaySocialInputs)}
+                  >
+                    Social Network Links
+                  </Button>
+                </div>
+                {displaySocialInputs ? (
+                  <CreateUpdateSocialMediaInputs
+                    handleValueChange={handleValueChange}
+                    social={profile.social}
+                  />
+                ) : (
+                  <Fragment></Fragment>
+                )}
+                <Button type="submit" variant="contained" color="primary">
+                  Submit
+                </Button>
+                <Typography>
+                  <Link href="/dashboard">Back</Link>
+                </Typography>
+              </form>
+            </Container>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
