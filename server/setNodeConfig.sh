e7f7fcb38ed2561daf631c2d1c409bb601e0c57e
João Pedro Paulo <joaopospaulo@gmail.com>
2020-08-02 09:13:50 +0100

    Changes a few things for Heroku deployment testing.

--- server/setNodeConfig.sh
+++ server/setNodeConfig.sh
@@ -4,8 +4,6 @@ set -e
 if [ -z ${PROD_DB_URI} ] || [ -z ${PROD_JWT_SECRET} ]; then
     exit -1
 else
-    echo ${PROD_DB_URI}
-    echo ${PROD_JWT_SECRET}
 	export NODE_CONFIG="{\"mongoURI\":\"${PROD_DB_URI}\", \"jwtSecret\":\"${PROD_JWT_SECRET}\"}"
 fi
 
